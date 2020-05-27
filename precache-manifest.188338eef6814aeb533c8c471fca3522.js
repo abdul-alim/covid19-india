ef7c48ce31acb3d05025a59c3b0780f27b1ecb34
Abdul Alim <abdul.alim@zohocorp.com>
2020-05-27 12:57:06 +0530

    Updates

---
+++ precache-manifest.188338eef6814aeb533c8c471fca3522.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34c2ddd25e138e3df70b1fcb4fd96121",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cba370f6e779c4907a76",
+    "url": "/static/css/main.02692209.chunk.css"
+  },
+  {
+    "revision": "c00b30e58d026d245c21",
+    "url": "/static/js/2.5095c8fe.chunk.js"
+  },
+  {
+    "revision": "d09ac505fec0e9130a1d5b85fb3fa6dc",
+    "url": "/static/js/2.5095c8fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cba370f6e779c4907a76",
+    "url": "/static/js/main.ca54c2aa.chunk.js"
+  },
+  {
+    "revision": "2bd651951091bd2a3ba3",
+    "url": "/static/js/runtime-main.4e7aff86.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e4c413ac8510f0ffbb494a4d71fb0237.js"
+  "/precache-manifest.188338eef6814aeb533c8c471fca3522.js"
 );
 
 self.addEventListener('message', (event) => {
